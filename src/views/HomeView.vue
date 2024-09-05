465f07f6413b54ab9813a2ee7d7203bc1a4e0aa7
Assem Morsy <assemmorsy@outlook.com>
2024-09-05 13:23:11 +0300

    add stream view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,25 @@
 <script setup lang="ts">
-import * as signalR from "@microsoft/signalr";
-import { onMounted, ref } from "vue";
-
-const connection = new signalR.HubConnectionBuilder()
-  .withUrl("https://sam-baloot-admin.online/saudi-baloot-olympics/dev/bracket-hub", {
-    withCredentials: true
-  })
-  .build();
-
-onMounted(async () => {
-  try {
-    await connection.start();
-    gameString.value = await connection.invoke('GetGroupBracket', 17);
-  }
-  catch (err) {
-    console.error(err);
-  }
-
-  connection.on("BracketChanged", (groupId: number, groupData: string) => {
-    console.log(groupData);
-    gameString.value = groupData;
-  });
-})
-
-const gameString = ref<null | string>(null);
 </script>
 
+
 <template>
-  <main>
-    <RouterLink to="camera">go to camera</RouterLink>
-    <!-- {{ gameString }} -->
+  <main class="view">
+    <!-- <RouterLink to="camera">go to camera</RouterLink> -->
+    <p class="paragraph">Hello</p>
   </main>
 </template>
+
+<style>
+.view {
+  height: 1920px;
+  width: 1080px;
+  background-color: transparent;
+
+  border: 2px darkcyan;
+}
+
+.paragraph {
+  color: aquamarine;
+  background-color: wheat;
+}
+</style>
